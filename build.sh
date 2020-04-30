edc66e8214bce75a40863d6f17c3266c757cfada
netdex <admin@netdex.cf>
2020-04-29 23:25:18 -0400

    ci: blacklist main.cpp for gcov

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 BUILD=build
+PROJECT=`pwd`
 
 cmake --version
 cmake -S . -B $BUILD -DCMAKE_BUILD_TYPE=Debug -DPACKAGE_TESTS=ON -DCODE_COVERAGE=ON
@@ -12,7 +13,13 @@ lcov -c -i -d . -o base_coverage.info
 ctest -j $(nproc) --output-on-failure
 lcov --directory . --capture --output-file test_coverage.info
 lcov -a base_coverage.info -a test_coverage.info -o coverage.info
-lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' '*/tests/*' '*/vendor/*' --output-file coverage.info
+lcov --remove coverage.info \
+	'/usr/*' \
+	"${HOME}"'/.cache/*' \
+	"$PROJECT/tests/*" \
+	"$PROJECT/vendor/*" \
+	"$PROJECT/src/main.cpp" \
+	--output-file coverage.info
 lcov --list coverage.info
 if [ ${CI:-false} = true ]; then
 	bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
