1e686ed4d29096bde8b8ef75efaa16f70226f65a
netdex <admin@netdex.cf>
2020-04-29 22:22:58 -0400

    ci: add coverage tooling

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-cmake --version
-cmake -S . -B build -DPACKAGE_TESTS=ON
-cmake --build build
-cd build
-ctest
 
+set -euo pipefail
+
+cmake --version
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DPACKAGE_TESTS=ON -DCODE_COVERAGE=ON
+cmake --build build -- -j $(nproc)
+cd build
+ctest -j $(nproc) --output-on-failure
--- tests/world/block_unittests.cpp
+++ tests/world/block_unittests.cpp
@@ -6,7 +6,5 @@ TEST(BlockTests, DirectionReflexiveInverse)
 {
 	using namespace world;
 	for (auto dir : kDirections)
-	{
 		EXPECT_EQ(dir, ord_to_dir_inv(dir_to_ord(ord_to_dir_inv(dir_to_ord(dir)))));
-	}
 }
