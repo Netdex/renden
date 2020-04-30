572ad57734414e59aeeb62794a49812d034f4bd9
netdex <admin@netdex.cf>
2020-04-29 22:35:29 -0400

    ci: add initial coverage

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ cmake --version
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DPACKAGE_TESTS=ON -DCODE_COVERAGE=ON
 cmake --build build -- -j $(nproc)
 cd build
+lcov -c -i -d . -o base_coverage.info
 ctest -j $(nproc) --output-on-failure
